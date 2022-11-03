b4d36889f720857485ebc4b14363f08addef25ae
CZY <113777049+VACUOU5@users.noreply.github.com>
2022-11-03 20:57:03 +0800

    Accelerometer for mouse

---
+++ WatchMouse/Accelerometer.ino
@@ -0,0 +1,106 @@
+void Accelerometer(){
+  accel_sensor.read();//This function gets new data from the acccelerometer
+
+  // Get the acceleration values from the sensor and store them into global variables
+  // (Makes reading the rest of the program easier)
+  x = accel_sensor.X;
+  y = accel_sensor.Y;
+  z = accel_sensor.Z;
+  temp = ((accel_sensor.rawTemp * 0.5) + 24.0);
+
+  // If the BMA250 is not found, nor connected correctly, these values will be produced
+  // by the sensor 
+  if (x == -1 && y == -1 && z == -1) {
+    // Print error message to Serial Monitor
+    SerialMonitorInterface.print("ERROR! NO BMA250 DETECTED!");
+  }
+  
+  else { // if we have correct sensor readings: 
+             int stay = 0;
+             int multiple=15;
+             int tempx,tempy;
+    if(x<-206){
+    tempx = -5*multiple;
+    }
+    else if(x<-156){
+      tempx=-4*multiple;
+    }
+    else if(x<-106){
+      tempx=-3*multiple;
+    }
+    else if(x<-56){
+      tempx=-2*multiple;
+    }
+    else if(x>=-56&&x<=56){
+      tempx=stay;
+    }
+     else if(x>56){
+      tempx= 2*multiple;
+    }
+     else if(x>106){
+      tempx= 3*multiple;
+    }
+      else if(x>156){
+      tempx= 4*multiple;
+    }
+    else if(x>206){
+      tempx = 5*multiple;
+    }
+
+
+
+    if(y<-206){
+      tempy=-5*multiple;
+    }
+    else if(y<-156){
+      tempy=-4*multiple;
+    }
+    else if(y<-106){
+      tempy=-3*multiple;
+    }
+    else if(y<-56){
+      tempy=-2*multiple;
+    }
+    else if(y>=-56&&y<=56){
+      tempy=stay;
+    }
+    else if(y>56){
+      tempy=2*multiple;
+    }
+    else if(y>106){
+      tempy=3*multiple;
+    }
+    else if(y>156){
+      tempy=4*multiple;
+    }
+    else if(y>206){
+      tempy=5*multiple;
+    }
+    updateMouseXY(tempx,tempy);
+  }
+  // The BMA250 can only poll new sensor values every 64ms, so this delay
+  // will ensure that we can continue to read values
+  delay(100);
+  // ***Without the delay, there would not be any sensor output*** 
+}
+/*
+uint8_t updateMouseXY(int8_t offsetX,int8_t offsetY){
+  
+  int8_t currX = (int8_t)curr_mouse_report[2];
+  int8_t currY = (int8_t)curr_mouse_report[1];
+
+  int8_t newX = offsetX + currX;
+  int8_t newY = offsetY*-1 + currY;
+  //overflow: -ve + -ve give positive
+  if (offsetX < 0 && currX < 0 && newX > 0){ newX = -128;}
+  //overflow: +ve + +ve give negative
+  else if (offsetX > 0 && currX > 0 && newX < 0) {newX = 127;}
+
+  if (offsetY < 0 && currY < 0 && newY > 0){ newY = -128;}
+  else if (offsetY > 0 && currY > 0 && newY < 0) {newY = 127;}
+
+  curr_mouse_report[2] = newX;
+  curr_mouse_report[1] = newY;
+
+  //ble_call_update = 40;
+}*/
--- WatchMouse/WatchMouse.ino
+++ WatchMouse/WatchMouse.ino
@@ -5,10 +5,23 @@
 #include <STBLE.h>
 #include "USBHID_Types.h"
 #include "UUID.h"
+#include "BMA250.h" 
+
+// Accelerometer sensor variables for the sensor and its values
+BMA250 accel_sensor;
+int x, y, z;
+double temp;
+
+#if defined(ARDUINO_ARCH_SAMD)
+ #define SerialMonitorInterface SerialUSB
+#else
+ #define SerialMonitorInterface Serial
+#endif
 
 #define SerialMonitorInterface SerialUSB
 
 TinyScreen display = TinyScreen(TinyScreenDefault);
+
 
 /*
 Bluetooth stuff.
@@ -19,8 +32,8 @@ uint8_t ble_connection_state = false;
 uint8_t curr_mouse_report[] = {0,0,0,0};
 uint8_t old_mouse_report[] = {0,0,0,0};
 
-uint8_t curr_media_report[] = {0,0};
-uint8_t old_media_report[] = {0,0};
+uint8_t curr_media_report[] = {0,0,0,0,0,0,0,0};
+uint8_t old_media_report[] = {0,0,0,0,0,0,0,0};
 
 
 long BLE_Timer = 0;
@@ -33,17 +46,20 @@ void setup() {
 
   Wire.begin();
   SerialMonitorInterface.begin(9600); //baud rate or something
-  while(!SerialMonitorInterface); //make sure we can print stuff in SMI, before we proceed
+  //while(!SerialMonitorInterface); //make sure we can print stuff in SMI, before we proceed
 
   display.begin();
   BLE_Setup();
 
+  SerialMonitorInterface.print("Initializing BMA...");
+  // Set up the BMA250 acccelerometer sensor
+  accel_sensor.begin(BMA250_range_2g, BMA250_update_time_64ms); 
 }
 
 void loop() {
   
   BLE_Loop();
-
+  Accelerometer();
 }
 
 /*
@@ -67,3 +83,4 @@ void HID_onBond(){
 /*
 End of bluetooth stuff
 */
+
