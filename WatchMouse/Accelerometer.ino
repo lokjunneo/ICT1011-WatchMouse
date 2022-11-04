1961f72b0d79089a5baf9880e450b6850d1fbd79
LokJunNeo <raizutiamat@gmail.com>
2022-11-05 02:14:55 +0800

    Added accel_cal_x and accel_cal_y for future calibration function.

--- WatchMouse/Accelerometer.ino
+++ WatchMouse/Accelerometer.ino
@@ -1,8 +1,11 @@
 
-int16_t x=0, y=0, z=0;
+int16_t accel_x=0, accel_y=0, accel_z=0;
 int tempx = 0,tempy = 0;
 long accel_millis = 0; //to delay when to read from sensor; 
 long update_millis = 0; //to delay updateMouse
+
+int accel_cal_x = 0, accel_cal_y = 0; //For storing calibration data of user
+
 void accelerometer_mouse(){
 long curr_millis = millis();
 
@@ -11,75 +14,77 @@ long curr_millis = millis();
     update_millis = 0;
     accel_millis = 0;
   }
+
   /* BMA only updates value every 70ms */
   if (curr_millis - accel_millis > 70){
     accel_sensor.read();//This function gets new data from the acccelerometer
-    x = accel_sensor.X;
-    y = accel_sensor.Y;
-    z = accel_sensor.Z;
+    accel_x = accel_sensor.X;
+    accel_y = accel_sensor.Y;
+    accel_z = accel_sensor.Z;
     accel_millis = millis();
   }
+  // Slows down update a little, which helps makes cursor movements feel smoother
   if ( curr_millis - update_millis > 10){
     /*  Make sure BMA250 remains detected */
-    if (x == -1 && y == -1 && z == -1) {
+    if (accel_x == -1 && accel_y == -1 && accel_z == -1) {
       SerialMonitorInterface.print("ERROR! NO BMA250 DETECTED!");
     }
     
     else { // if we have correct sensor readings: 
         
-      if(x<-206){
+      if(accel_x<-206+accel_cal_x){
         tempx = -10;
       }
-      else if(x<-156){
+      else if(accel_x<-156+accel_cal_x){
         tempx=-5;
       }
-      else if(x<-130){
+      else if(accel_x<-130+accel_cal_x){
         tempx=-3;
       }
-      else if(x<-80){
+      else if(accel_x<-80+accel_cal_x){
         tempx=-1;
       }
-      else if(x>=-80&&x<=80){
+      else if(accel_x>=-80+accel_cal_x && accel_x<=80+accel_cal_x){
         tempx=0;
       }
-      else if(x>206){
+      else if(accel_x>206+accel_cal_x){
         tempx = 10;
       }
-      else if(x>156){
+      else if(accel_x>156+accel_cal_x){
         tempx= 5;
       }
-      else if(x>130){
+      else if(accel_x>130+accel_cal_x){
         tempx= 3;
       }
-      else if(x>80){
+      else if(accel_x>80+accel_cal_x){
         tempx= 1;
       }
 
-      if(y<-206){
+      if(accel_y<-206+accel_cal_y){
         tempy=-10;
       }
-      else if(y<-156){
+      else if(accel_y<-156+accel_cal_y){
         tempy=-5;
       }
-      else if(y<-106){
+      else if(accel_y<-106+accel_cal_y){
         tempy=-3;
       }
-      else if(y<-36){
+      else if(accel_y<-36+accel_cal_y){
         tempy=-1;
       }
-      else if(y>=-36&&y<=36){
+      else if(accel_y>=-36+accel_cal_y && accel_y<=36 + accel_cal_y){
         tempy=0;
       }
-      else if(y>206){
+      else if(accel_y>206 + accel_cal_y){
         tempy=10;
       }
-      else if(y>156){
+      else if(accel_y>156 + accel_cal_y){
         tempy=5;
       }
-      else if(y>106){
+      else if(accel_y>106 + accel_cal_y){
         tempy=3;
       }
-      else if(y>36){
+      else if(accel_y>36 + accel_cal_y){
         tempy=1;
       }
 
